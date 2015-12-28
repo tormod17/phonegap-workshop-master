9d483d4fbf9f86adac2aacb4003298ae3fb2def5
tormod17 <tormodsmith@gmail.com>
2015-12-28 19:19:16 -0400

    add camera api

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -4,6 +4,7 @@ var EmployeeView = function() {
     this.initialize = function() {
         this.el = $('<div/>');
         this.el.on('click', '.add-location-btn', this.addLocation);
+        this.el.on('click', '.change-pic-btn', this.changePicture);
     };
 
 
@@ -25,7 +26,32 @@ var EmployeeView = function() {
         return false;
     };
 
-   
+    this.changePicture = function(event) {
+        event.preventDefault();
+        if (!navigator.camera) {
+            app.showAlert("Camera API not supported", "Error");
+            return;
+        }
+        var options = {
+            quality: 50,
+            destinationType: Camera.DestinationType.DATA_URL,
+            sourceType: 1, // 0:Photo Library, 1=Camera, 2=Saved Photo Album
+            encodingType: 0 // 0=JPG 1=PNG
+        };
+
+        navigator.camera.getPicture(
+            function(imageData) {
+                $('.employee-image', this.el).attr('src', "data:image/jpeg;base64," + imageData);
+            },
+            function() {
+                app.showAlert('Error taking picture', 'Error');
+            },
+            options);
+
+        return false;
+    };
+
+
 
     this.initialize();
 
